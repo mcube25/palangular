d9b06dc8a77e11dfa74a7ad6c93fbdf702fc426f
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-02-21 12:23:36 -0800

    component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'my-new-app';
-  ready =  'hello world !!!';
+  //name = 'my-new-app';
+  //ready =  'hello world !!!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import {ServerComponent} from './server/server.component'; 
+import {ButtonComponent} from './button/button.component'; 
+//import {httpModule} from '@angular/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+   FormsModule,
+   //httpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  name = "palemc"
- }
+export class AppModule {}
