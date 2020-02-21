9d0be1f686321329f5cb7ae26d4d77625fbd4e14
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-02-21 10:20:18 -0800

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-new-app';
+  name = 'my-new-app';
+  ready =  'hello world !!!';
 }
