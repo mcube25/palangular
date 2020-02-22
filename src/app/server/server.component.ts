96096b22edc036cfd75bb4ee56a72f6c18534694
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-02-21 17:28:19 -0800

    styling attribuutes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,26 +6,31 @@ import {Component, OnInit} from '@angular/core'
     styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit{
-
+    serverID =10;
+    serverStatus: string ='offline';
+    comeOnLine: string ='click below'
 allowNewServer = false;
 serverName = 'hello';
 serverCreationStatus ='no server was created';
+serverCreated = false;
     constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online':'offline';
         setTimeout(()=>{
       this.allowNewServer = true;
         }, 2000)
     }
 ngOnInit(){}
 onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus='server was created !!'+ this.serverName};
     onUpdateServerName(event: any){
     this.serverName = (<HTMLInputElement>event.target).value;
     }
-    serverID =10;
-serverStatus: string ='offline';
-comeOnLine: string ='click below'
 
 getServerStatus(){
     return this.comeOnLine;
+}
+getColor(){
+    return this.serverStatus === 'online'? 'green':'red';
 }
 }
